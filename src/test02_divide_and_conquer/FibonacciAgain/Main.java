f16de3ff1cf72ee851faa2fe599b3c898bece70c
Aye10032 <aye10032@gmail.com>
2023-10-20 14:19:22 +0800

    UVA10276

---
+++ src/test02_divide_and_conquer/FibonacciAgain/Main.java
@@ -0,0 +1,74 @@
+package test02_divide_and_conquer.FibonacciAgain;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/**
+ * @program: OJ
+ * @description: HDU 1021
+ * @author: Aye10032
+ * @create: 2023-10-20 13:11
+ **/
+
+class Reader {
+    static BufferedReader reader;
+    static StringTokenizer tokenizer;
+
+    /**
+     * call this method to initialize reader for InputStream
+     */
+    static void init(InputStream input) {
+        reader = new BufferedReader(
+                new InputStreamReader(input));
+        tokenizer = new StringTokenizer("");
+    }
+
+    /**
+     * get next word
+     */
+    static String next() throws IOException {
+        while (!tokenizer.hasMoreTokens()) {
+            tokenizer = new StringTokenizer(
+                    reader.readLine());
+        }
+        return tokenizer.nextToken();
+    }
+
+    static int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+}
+
+public class Main {
+
+    Main() {
+
+    }
+
+    void run() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            int n = scanner.nextInt();
+
+            System.out.println(n % 4 == 2 ? "yes" : "no");
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        Main demo = new Main();
+
+        demo.run();
+    }
+
+}
