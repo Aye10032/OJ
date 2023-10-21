2cdb4a2802e99ccab2b0f9489d94102b9f06a565
Aye10032 <aye10032@gmail.com>
2023-10-21 20:49:46 +0800

    HDU 6882

---
+++ src/test02_divide_and_conquer/DivideAndConquer/Main.java
@@ -0,0 +1,68 @@
+package test02_divide_and_conquer.DivideAndConquer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @program: OJ
+ * @description: HDU 6882
+ * @author: Aye10032
+ * @create: 2023-10-20 14:25
+ **/
+
+class Reader {
+    static BufferedReader reader;
+    static StringTokenizer tokenizer;
+
+    /**
+     * call this method to initialize reader for InputStream
+     */
+    static void init(InputStream input) {
+        reader = new BufferedReader(
+                new InputStreamReader(input));
+        tokenizer = new StringTokenizer("");
+    }
+
+    /**
+     * get next word
+     */
+    static String next() throws IOException {
+        while (!tokenizer.hasMoreTokens()) {
+            tokenizer = new StringTokenizer(
+                    reader.readLine());
+        }
+        return tokenizer.nextToken();
+    }
+
+    static int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+}
+
+
+class Point{
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
+public class Main {
+}
