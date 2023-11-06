0eec43e86022d976a32ac87aa92b5203dbcda468
Aye10032 <aye10032@gmail.com>
2023-11-06 19:41:39 +0800

    update

--- src/test02_divide_and_conquer/DivideAndConquer2/Main.java
+++ src/test02_divide_and_conquer/DivideAndConquer2/Main.java
@@ -1,5 +1,11 @@
 package test02_divide_and_conquer.DivideAndConquer2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /**
  * @program: OJ
  * @description:
@@ -10,4 +16,59 @@ package test02_divide_and_conquer.DivideAndConquer2;
 
 
 public class Main {
+    Main(){
+
+    }
+
+    void run() throws IOException {
+        Reader.init(System.in);
+
+        int m = Reader.nextInt();
+
+
+    }
+
+    public static void main(String[] args) {
+        Main demo = new Main();
+
+        try {
+            demo.run();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
+
+class Reader {
+    static BufferedReader reader;
+    static StringTokenizer tokenizer;
+
+    /**
+     * call this method to initialize reader for InputStream
+     */
+    static void init(InputStream input) {
+        reader = new BufferedReader(
+                new InputStreamReader(input));
+        tokenizer = new StringTokenizer("");
+    }
+
+    /**
+     * get next word
+     */
+    static String next() throws IOException {
+        while (!tokenizer.hasMoreTokens()) {
+            tokenizer = new StringTokenizer(
+                    reader.readLine());
+        }
+        return tokenizer.nextToken();
+    }
+
+    static int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
 }
